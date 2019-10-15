53faaee677f9f6814af4ff88cd3ed8a9818222de
stevenxu9627 <stevenxu9627@gmail.com>
2019-10-14 21:21:33 -0400

    passed 1st

--- src/callbacker.js
+++ src/callbacker.js
@@ -1,5 +1,27 @@
-const syncCallbacker = null;
+// const syncCallbacker = null;
 
-const asyncCallbacker = null;
+// const asyncCallbacker = null;
+
+function a(){};
+function b() {};
+function syncCallbacker(a, b){
+    if(typeof a !== 'function' || typeof b !== 'function'){
+        throw 'error';
+    }
+    let aResult = a();
+    let bResult = b(aResult);
+    return bResult;
+}
+
+
+function asyncCallbacker(a,b){
+    if(typeof a !== 'function' || typeof b !== 'function'){
+        throw 'error';
+    }
+    const done = (data) => {
+        b(data,done);
+    }
+    a(data = null,done)
+}
 
 module.exports = { syncCallbacker, asyncCallbacker };
